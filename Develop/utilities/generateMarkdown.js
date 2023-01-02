3c456034ab8a60f909da5792b703457f05fa33a9
colint771 <colint771@gmail.comgit config --global user.email colint771@gmail.com>
2023-01-02 13:51:39 -0500

    added getmarkdown.js code

--- Develop/utilities/generateMarkdown.js
+++ Develop/utilities/generateMarkdown.js
@@ -1,19 +1,96 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+    if (license !== 'no license') {
+        return `![badge](https://img.shields.io/badge/license-${license}-blue)`;
+    } else {
+        return ' ';
+    }
+}
 
-// TODO: Create a function that returns the license link
+// Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+    if (license !== 'no license') {
+        return `[${license}](https://choosealicense.com/licenses/${license})`;
+    } else {
+        return ' ';
+    }
+}
 
-// TODO: Create a function that returns the license section of README
+// function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    if (license !== 'no license') {
+        return ` `;
+    } else {
+        return ' ';
+    }
+}
 
-// TODO: Create a function to generate markdown for README
+// function that return license in table of contents
+function renderLicenseTOC(license) {
+    if (license !== 'no license') {
+        return `* [License](#license)`;
+    } else {
+        return ' ';
+    }
+}
+
+//function for returning a message for not wanting contributors
+function renderContributingSection(confirmContributers, data) {
+    if (!confirmContributers) {
+      return `
+    Thank you for your interest contributing; however, right now, I will not be accepting outside contributions.
+      `;
+    } else {
+      return `
+    ${data}
+      `;
+    }
+  }
+
+// function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+  ${renderLicenseBadge(data.license)}
+
+  ## Table-of-Contents
+
+  * [Description](#description)
+  * [Installation](#installation)
+  * [Usage](#usage)
+  ${renderLicenseTOC(data.license)}
+  * [Contributing](#contributing)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## [Description](#table-of-contents)
+
+  ${data.description}
+
+  ## [Installation](#table-of-contents)
+
+  ${data.installation}
+
+  ## [Usage](#table-of-contents)
+
+  ${data.usage}
+  
+  ${renderLicenseSection(data.license)}
+
+  ## [Contributing](#table-of-contents)
+  
+  ${renderContributingSection(data.confirmContributers, data.contribute)}
+
+  ## [Questions](#table-of-contents)
+
+  Please contact me using the following links:
+
+  [GitHub](https://github.com/${data.githubUsername})
+
+  [Email: ${data.email}](mailto:${data.email})
 `;
 }
 
