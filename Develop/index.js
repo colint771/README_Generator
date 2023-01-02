d6f2bbc3eb6cc91b4a33e208373736a19db08aeb
colint771 <colint771@gmail.comgit config --global user.email colint771@gmail.com>
2023-01-02 13:22:29 -0500

    index.js code created

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,160 @@
-// TODO: Include packages needed for this application
+// packages needed for this application
+const inquirer = require('inquirer');
+const generateMarkdown = require('Develop/utilities/generateMarkdown.js');
+const fs = require('fs');
 
-// TODO: Create an array of questions for user input
-const questions = [];
+// Array of questions for user input
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a title!');
+                return false;
+            }
+        }
+    },
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'What is your GitHub Username? (Required)',
+        validate: githubInput => {
+            if(githubInput) {
+                return true;
+            } else {
+                console.log('Please enter your GitHub Username!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address? (Required)',
+        validate: emailInput => {
+            if(emailInput) {
+                return true;
+            } else {
+                console.log('Please enter your email address!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'Description',
+        message: 'Provide a description for your project, including what it is, why it is needed, and how it works (Required)',
+        validate: descriptionInput => {
+            if(descriptionInput) {
+                return true;
+            } else {
+                console.log('Please enter a description for your project!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Please provide instructions for installing your project. (Required)',
+        validate: installInput => {
+            if (installInput) {
+                return true;
+            } else {
+                console.log('Please provide installation instructions!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Please provide instructions for using the application. (Required)',
+        validate: usageInput => {
+            if(usageInput) {
+                return true;
+            } else {
+                console.log('Please enter usage instructions!');
+                return false;
+            }
+        }
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Which license will you use for your peroject?',
+        choices: ['mit', 'agpl', 'apache', 'no license']
+    },
+    {
+        type: 'confirm',
+        name: 'confirmContributers',
+        message: 'Do you want to allow other developers to contribute?',
+        default: true
+    },
+    {
+        type: 'input',
+        name: 'contributer',
+        message: 'Please provide guidlines for contributing. (Required)',
+        when: ({ confirmContributers }) => {
+            if (confirmContributers) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        validate: contributerInput => {
+            if (contributerInput) {
+                return true;
+            } else {
+                console.log('Please enter guidlines for contributers!');
+                return false; 
+            }
+        }
+    }
+];
 
-// TODO: Create a function to initialize app
-function init() {}
+// function to write README file
+const writeFile = fileContent => {
+    return new Promise((resolve, reject) => {
+        fs.writeFIle('./outputs/generated-README.md', fileContent, err => {
+            if (err) {
+                reject(err);
+                return;
+            }
+
+            resolve({
+                ok: true,
+                message: 'File Created!'
+            });
+        });
+    });
+};
+
+// Create a function to initialize app
+const init = () => {
+    return inquirer.prompt(questions)
+    .then(readmeData => {
+        return readmeData;
+    })
+}
 
 // Function call to initialize app
-init();
+init()
+.then(readmeData => {
+    console.log(readmeData);
+    return generateMarkdown(readmeData);
+})
+.then(pageMD => {
+    return writeFIle(pageMD);
+})
+.then(writeFileResponse => {
+    console.log(writeFileResponse.message);
+})
+.catch(err => {
+    console.log(err);
+})
